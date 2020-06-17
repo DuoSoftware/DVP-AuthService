5b032c6ac0cf2788378b03b40b89eac1c1895cd3
coderaduo <duoacoder@gmail.com>
2020-06-17 08:39:59 +0530

    Update default.js

--- config/default.js
+++ config/default.js
@@ -114,7 +114,7 @@ module.exports = {
     notificationServiceHost: "notificationservice.app1.veery.cloud",
     notificationServicePort: "8089",
     notificationServiceVersion: "1.0.0.0",
-    dynamicPort : false
+    dynamicPort : true
   },
 
   Tenant: {
