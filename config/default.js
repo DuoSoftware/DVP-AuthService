373df4442e809bc5b85aa23a4f27292b24da9654
Rushaid <rushaid@duosoftware.com>
2021-06-17 20:38:57 +0530

    Update default.js
    check concurrent limit activated

--- config/default.js
+++ config/default.js
@@ -49,7 +49,7 @@ module.exports = {
 
     ui_host: "http://localhost:3000/",
     agent_host: "http://localhost:3000/",
-    check_concurrent_limit: false,
+    check_concurrent_limit: true,
     multi_login: false,
   },
   Redis: {
